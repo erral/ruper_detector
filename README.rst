934e5a8a731871c7371bee3152dc1c13180159eb
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-11-07 08:05:30 +0100

    docs

---
+++ README.rst
@@ -0,0 +1,15 @@
+ruper_detector
+===============
+
+Gure bulegoan askotan edukitzen dugu `EITB Irratia`_ jarrita. Eta askotan
+esan ohi dugu Ruper Ordorikarenganako lerratze bat duela irrati horrek.
+
+Gainera gure bulegoa Ruperren fanez beteta dago, eta beste batzuri logurea
+besterik ez digu ematen.
+
+Programatxo honek, egunaren zein ordutan lo hartuko dugun abisatzen digu
+Ruper noiz jarriko duten esanez.
+
+Broma bat da, hartu umorez!
+
+.. _`EITB Irratia`: http://www.eitb.com/eu/irratia/eitb-musika/
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
-import sys, os
 
 version = '1.0'
 
 setup(name='ruper_detector',
       version=version,
-      description="",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      description="EITB Irratian Ruper Ordorikaren kantuen detektorea",
+      long_description=open("README.rst").read() + "\n" +
+                       open("HISTORY.txt").read(),
+
+      classifiers=[],
       keywords='',
       author='',
       author_email='',
@@ -19,8 +19,11 @@ setup(name='ruper_detector',
       zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
+          'beautifulsoup4'
       ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+      entry_points={
+        'console_scripts': [
+              'ruper_detector = ruper.main:main',
+        ]
+      },
       )
