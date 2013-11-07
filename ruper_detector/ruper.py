3e57334bf2e5205f1a426d810930a2eb18981b58
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-11-07 08:28:54 +0100

    minor changes in parsing

--- ruper_detector/ruper.py
+++ ruper_detector/ruper.py
@@ -6,25 +6,35 @@ import urllib2
 
 EITB_MUSIKA_URL = 'http://www.eitb.com/es/modulo/radio/eitbmusika_2col_ajax/?station=1&date={0}&hour={1}'
 
-RUPER = u'RUPER ORDORIKA'
+RUPER = u'RUPER'
 
 
 def detektorea(eguna):
     """ Detektatu Ruper! """
     ruperren_kantak = []
+    print 'Ruper detektatzen...'
     for int_ordua in range(24):
-        ordua = '{0:02}'.format(1)
+        ordua = '{0:02}'.format(int_ordua)
         url = EITB_MUSIKA_URL.format(eguna, ordua)
         sock = urllib2.urlopen(url)
         soup = BeautifulSoup(sock.read())
         for kanta in soup.find_all('tr'):
-            for zutabe in kanta.find_all('td'):
-                if RUPER in zutabe[1].text:
+            zutabeak = kanta.find_all('td')
+            if len(zutabeak) == 3:
+                if RUPER in zutabeak[1].text:
                     ruperren_kantak.append(dict(
-                        ordua=zutabe[0].text,
-                        artista=zutabe[1].text,
-                        kanta=zutabe[2].text,
+                        ordua=zutabeak[0].text,
+                        artista=zutabeak[1].text,
+                        kanta=zutabeak[2].text,
                         ))
+
+    print eguna
+    if len(ruperren_kantak) == 1:
+        print 'Gaur Ruper behin bakarrik entzungo dugu. Apuntatu agendan!'
+    elif len(ruperren_kantak) == 0:
+        print 'Ohhh! Gaur ez dute Ruper jarriko!!!'
+    else:
+        print 'Ondo!!! Gaur Ruper {0} aldiz entzungo dugu!! Hauek dira orduak:'.format(len(ruperren_kantak))
 
     for kanta in ruperren_kantak:
         print kanta['ordua'], kanta['kanta']
